404b0f29df35e8ab9895510f55e903b40dd2829c
Cathy Leung <cathyleungg@gmail.com>
2017-07-07 13:44:24 -0700

    changed layout

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 /**
  * Created by cathyleung on 2017-04-25.
  */
+/*
+var aboutMe = document.getElementById("about-me-text");
+aboutMe.style.marginTop = $("#menu").outerHeight() + "px";
+console.log(aboutMe.style.marginTop);
+*/
+
+
