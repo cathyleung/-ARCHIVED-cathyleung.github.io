4664660c34f8d12cdfd817964578a4283d3fe156
Cathy Leung <cathyleungg@gmail.com>
2017-07-09 23:31:55 -0700

    added links

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ aboutMe.style.marginTop = $("#menu").outerHeight() + "px";
 console.log(aboutMe.style.marginTop);
 */
 
+$(".proj1-info").hide();
+$("#proj1-label").click(function() {
+    if( $(".proj1-info").is(":visible")) {
+        $(".proj1-info").hide();
+    } else {
+        $(".proj1-info").show();
+        $(".proj2-info").hide();
+    }
+});
+
+$(".proj2-info").hide();
+$("#proj2-label").click(function() {
+    if( $(".proj2-info").is(":visible")) {
+        $(".proj2-info").hide();
+    } else {
+        $(".proj2-info").show();
+        $(".proj1-info").hide();
+    }
+});
+
 
